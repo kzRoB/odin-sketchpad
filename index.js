37515e9b533966e82243fc51b5beef81b938b9aa
Kevin <168787179+kzRoB@users.noreply.github.com>
2024-09-09 18:26:09 +0000

    Added proper grid using flexbox, along with borders

--- index.js
+++ index.js
@@ -1,35 +1,42 @@
-const gridSize = 32
+const gridSize = 16
 
-//testing
-//document.body.style.color = "rgb(255,255,255)"
-//document.body.style.backgroundColor = "rgb(0,0,0)"
-
-//MouseDown
-var mouseDown = false
-document.body.onmousedown = function() { 
-    mouseDown = true
-}
-document.body.onmouseup = function() {
-    mouseDown = false
+function centerDiv(object) {
+    object.style.marginLeft = "auto"
+    object.style.marginRight = "auto"
 }
 
 //Create the drawing board
 const grid = document.createElement("div")
-grid.style.width = "800px"
-grid.style.height = "800px"
-grid.style.gap = "4px"
-grid.style.display = "grid"
-grid.style.gridTemplateColumns = "repeat("+gridSize+","+100/gridSize+"%)"
-grid.style.gridTemplateRows = "repeat("+gridSize+","+100/gridSize+"%)"
 grid.style.backgroundColor = "rgb(200,200,200)"
+grid.style.width = "480px"
+grid.style.height = "480px"
+centerDiv(grid)
+grid.style.display = "flex"
+grid.style.flexDirection = "row"
+grid.style.flexWrap = "wrap"
 document.body.appendChild(grid)
 
-//Create each square in the drawing board
-for (let i=0; i<gridSize**2; i++) {
-    let box = document.createElement("div")
-    box.style.backgroundColor = "rgb(255,255,255)"
-    box.addEventListener("mouseover", () => {
-        box.style.backgroundColor = "rgb(0,0,0)"
-    })
-    grid.appendChild(box)
+//Create each column
+for (let i=0; i<gridSize; i++) {
+    let column = document.createElement("div")
+    column.style.backgroundColor = "rgb(255,255,255)"
+    column.style.display = "flex"
+    column.style.flexDirection = "column"
+    column.style.flex = 1
+    column.style.flexWrap = "wrap;"
+    grid.appendChild(column)
+
+    //Create each row
+    for (let i=0; i<gridSize; i++) {
+        let row = document.createElement("div")
+        row.style.backgroundColor = "rgb(255,255,255)"
+        row.style.border = "solid"
+        row.style.borderWidth = "1px"
+        row.style.flexBasis = "100%"
+        row.style.flex = 1
+        row.addEventListener("mouseover", () => {
+            row.style.backgroundColor = "rgb(0,0,0)"
+        })
+        column.appendChild(row)
+    }
 }
